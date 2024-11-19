a39b70a809a917e398b1146fd3996a33ce69843c
Matt Brictson <matt@mattbrictson.com>
2024-11-19 13:35:44 -0800

    Extend AS::Configurable to add :from_env to config_accessor

--- lib/configurable_from_env.rb
+++ lib/configurable_from_env.rb
@@ -7,4 +7,25 @@ require "active_support/core_ext/hash/keys"
 module ConfigurableFromEnv
   autoload :EnvironmentValue, "configurable_from_env/environment_value"
   autoload :VERSION, "configurable_from_env/version"
+
+  extend ActiveSupport::Concern
+  include ActiveSupport::Configurable
+
+  module ClassMethods
+    def config_accessor(*attributes, from_env: nil, **options, &block)
+      if from_env && attributes.many?
+        raise ArgumentError, "Only one accessor at a time can be created using the :from_env option"
+      end
+
+      env_value = EnvironmentValue.from(from_env)
+      accessor = super(*attributes, **options, &block)
+      default_provided = options.key?(:default) || block_given?
+
+      env_value&.read(required: !default_provided) do |value|
+        public_send(:"#{attributes.first}=", value)
+      end
+
+      accessor
+    end
+  end
 end
--- test/configurable_from_env_test.rb
+++ test/configurable_from_env_test.rb
@@ -1,7 +1,73 @@
 require "test_helper"
 
 class ConfigurableFromEnvTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::ConfigurableFromEnv::VERSION
+  def setup
+    @class = Class.new
+    @class.include ConfigurableFromEnv
+  end
+
+  def teardown
+    ENV.delete("TEST_API_KEY")
+    ENV.delete("TEST_TIMEOUT")
+  end
+
+  def test_defines_accessor_with_no_env_source
+    @class.config_accessor :logger
+
+    assert_respond_to @class, :logger
+    assert_respond_to @class, :logger=
+  end
+
+  def test_defines_multiple_accessors_with_no_env_source
+    @class.config_accessor :http_client, :logger
+
+    assert_respond_to @class, :http_client
+    assert_respond_to @class, :http_client=
+    assert_respond_to @class, :logger
+    assert_respond_to @class, :logger=
+  end
+
+  def test_raises_if_multiple_accessors_declared_with_one_env_option
+    error = assert_raises(ArgumentError) { @class.config_accessor :api_key, :logger, from_env: "TEST_API_KEY" }
+    assert_match "Only one accessor at a time can be created using the :from_env option", error.message
+  end
+
+  def test_defines_accessor_with_key_and_implicit_type
+    @class.config_accessor :api_key, from_env: "TEST_API_KEY", default: nil
+
+    assert_respond_to @class, :api_key
+    assert_respond_to @class, :api_key=
+  end
+
+  def test_defines_accessor_with_key_and_type
+    @class.config_accessor :timeout, from_env: { key: "TEST_TIMEOUT", type: :integer }, default: 30
+
+    assert_respond_to @class, :timeout
+    assert_respond_to @class, :timeout=
+  end
+
+  def test_uses_default_value_when_env_var_is_absent
+    @class.config_accessor :timeout, from_env: { key: "TEST_TIMEOUT", type: :integer }, default: 30
+
+    assert_equal 30, @class.timeout
+  end
+
+  def test_assigns_integer_env_var_value_overriding_default
+    ENV["TEST_TIMEOUT"] = "5"
+    @class.config_accessor :timeout, from_env: { key: "TEST_TIMEOUT", type: :integer }, default: 30
+
+    assert_equal 5, @class.timeout
+  end
+
+  def test_assigns_string_env_var_value_with_no_default
+    ENV["TEST_API_KEY"] = "secret"
+    @class.config_accessor :api_key, from_env: "TEST_API_KEY"
+
+    assert_equal "secret", @class.api_key
+  end
+
+  def test_raises_if_no_default_and_env_var_is_absent
+    error = assert_raises(ArgumentError) { @class.config_accessor :api_key, from_env: "TEST_API_KEY" }
+    assert_match "Missing required environment variable: TEST_API_KEY", error.message
   end
 end
